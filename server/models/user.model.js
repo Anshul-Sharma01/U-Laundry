4a2f4e0220fae54a4f866eaa9d0ba6dac42e83d2
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-08-29 19:04:08 +0530

    Added User model
    Defined user schema with authentication, avatar, history tracking, and password hashing. Added fields for refresh tokens and password reset functionality.

---
+++ server/models/user.model.js
@@ -0,0 +1,106 @@
+import mongoose, { Schema } from "mongoose";
+import bcryptjs from "bcryptjs";
+
+
+const userSchema = new Schema({
+    username : {
+        type : String,
+        required : [true, "Username is required"],
+        unique : [true, "Username must be unique"],
+        minLength : [10, "Username must be of atleast 10 characters"],
+        trim : true
+    },
+    name : {
+        type : String,
+        required : [true, "Name is required"]
+    },
+    fatherName : {
+        type : String,
+        required : [true, "Father's name is required"]
+    },
+    email : {
+        type : String,
+        required : [true, "Email is required"],
+        unique : [true, "Email must be unique"],
+        match : [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Please enter a valid email address"]
+    },
+    password : {
+        type : String,
+        required : [true, "Password is required"],
+        match : [/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,"Password must contain at least one letter, one number, and one special character"],
+        select : false,
+    },
+    hostelName : {
+        type : String,
+        required : [true, "Hostel name is required !!"],
+    },
+    roomNumber : {
+        type : String,
+        required : [true, "Room number is required"],
+    },
+    avatar : {
+        public_id : {
+            type : String,
+            required : true,
+        },
+        secure_url : {
+            type : String,
+            required : true
+        }
+    },
+    degreeName : {
+        type : String,
+        enum : ['BCA','BE','PHARMA', 'NURS'],
+        default : 'BE',
+        required : [true, "Degree Name is required"]
+    },
+    role : {
+        type : String,
+        enum : ['student', 'admin', 'laundary-moderator'],
+        default : 'student',
+        select : false
+    },
+    history : [
+        {
+            moneyPaid : {
+                type : Number,
+                required : [true, "Payment amount is required"],
+            },
+            clothesWashed : {
+                type : Number,
+                required : [true, "Number of clothes washed is required"],
+            },
+            date : {
+                type : Date,
+                default : Date.now,
+                required : [true, "Date of service is required"],
+            },
+            pickedUpWhen : {
+                type : Date,
+                required : [true, "Pick-up date is required"]
+            }
+        }
+    ],
+    refreshToken : {
+        type : String
+    },
+    forgotPasswordToken : String,
+    forgotPasswordExpiry : Date,
+    
+},{
+    timestamps : true
+})
+
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified("password")){
+        return next();
+    }
+    this.password = await bcryptjs.hash(this.password, 10);
+    next();
+})
+
+
+
+export const User = mongoose.model('User', userSchema);
+
